01fc778e6d1c5695bfef33d0efce9f7d67d3ba14
Zinne0811 <nguyen.tinh.8113@gmail.com>
2024-04-03 18:10:24 +0700

    push project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from './layouts/NavBar';
+import Footer from './layouts/Footer';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar/>
+      <Footer/>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/layouts/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+          <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+              <div className='container-fluid'>
+                  <a href='/' className='navbar-brand'>HustCinema</a>
+              </div>
+          </nav>
+
+    </div>
+  )
+}
